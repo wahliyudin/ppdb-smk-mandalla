acc34de58cc6341dbceeae5b884d9ce79d5af017
wahliyudin <wahliyudinzein@gmail.com>
2023-06-08 10:41:14 +0700

    Struktur aplikasi

---
+++ app/Http/Controllers/Proses/CalonSiswaController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Proses;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class CalonSiswaController extends Controller
+{
+    public function index()
+    {
+        return view('');
+    }
+}
---
+++ app/Http/Controllers/Proses/PembayaranController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Proses;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class PembayaranController extends Controller
+{
+    //
+}
